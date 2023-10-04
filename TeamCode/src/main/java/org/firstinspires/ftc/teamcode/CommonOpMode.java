c386758a3368365997350baa6a310e19766fd9dd
henrykozlik <24hkozlik@ramsey.k12.nj.us>
2023-10-03 22:40:05 -0400

    Adding OpenCV and RoadRunner to the project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommonOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommonOpMode.java
@@ -1,15 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 public abstract class CommonOpMode extends LinearOpMode {
+    protected DcMotor leftFrontMotor;
+    protected DcMotor leftBackMotor;
+    protected DcMotor rightFrontMotor;
+    protected DcMotor rightBackMotor;
+
+    protected Gamepad gamepad;
 
     @Override
     public void runOpMode() {
         waitForStart();
 
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
+
+        gamepad = gamepad1;
+
+        gamepad.reset();
+        gamepad.rumble(500);
+
         while(opModeIsActive()) {
             telemetry.addData("IsOpModeActive", true);
+
+            telemetry.update();
 
             runner();
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualControlOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualControlOpMode.java
@@ -4,8 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Driving OpMode")
 public class ManualControlOpMode extends CommonOpMode {
+    final double CURVE = 2.5;
+    final double TURN_MAX = 0.5;
     @Override
     public void runner() {
+        double lx = gamepad.left_stick_x < 0 ? -(Math.pow(-gamepad.left_stick_x, CURVE)) : Math.pow(gamepad.left_stick_x, CURVE);
+        double ly = gamepad.left_stick_y < 0 ? -(Math.pow(-gamepad.left_stick_y, CURVE)) : Math.pow(gamepad.left_stick_y, CURVE);
+        double rx = (gamepad.right_stick_x < 0 ? -(Math.pow(-gamepad.right_stick_x, CURVE)) : Math.pow(gamepad.right_stick_x, CURVE)) * TURN_MAX;
 
+        leftFrontMotor.setPower(-lx - ly - rx);
+        leftBackMotor.setPower(lx - ly - rx);
+        rightFrontMotor.setPower(-lx + ly - rx);
+        rightBackMotor.setPower(lx + ly - rx);
     }
 }
